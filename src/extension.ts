f74410dfbe0fb50b5c003e5f6fb0d3c66a73f9cc
Oran9eUtan <Oran9eUtan@gmail.com>
2021-04-14 23:12:16 +0200

    Added refresh command for development

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,15 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('mcmodel-viewer.test', () => {
 		MCModelPanel.createOrShow(context.extensionUri);
 	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('mcmodel-viewer.refresh', () => {
+		MCModelPanel.kill();
+		MCModelPanel.createOrShow(context.extensionUri);
+
+		setTimeout(() => {
+			vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
+		}, 500);
+	}));
 }
 
 export function deactivate() {}
