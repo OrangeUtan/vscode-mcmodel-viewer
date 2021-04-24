326fefe747d4e70bc644861aaec74b9627afa926
Oran9eUtan <Oran9eUtan@gmail.com>
2021-04-24 22:50:07 +0200

    feat: Can now resolve assets taht include namespaces

--- src/extension/minecraft.ts
+++ src/extension/minecraft.ts
@@ -4,13 +4,11 @@ import * as fs from 'fs';
 import * as config from './config';
 
 export let assetsRoots: vscode.Uri[] = [];
-export let textureAssetsRoots: Array<vscode.Uri> = [];
 export let modelAssetsRoots: Array<vscode.Uri> = [];
 
 export async function updateAssetsRoots() {
     assetsRoots = (await findAssetsRootsInWorkspace()).concat(config.getAssetsRoots());
-	textureAssetsRoots = await findTextureAssetsRoots(assetsRoots);
-	modelAssetsRoots = await findModelAssetsRoots(assetsRoots);
+    modelAssetsRoots = await findModelAssetsRoots(assetsRoots);
 }
 
 /**
@@ -22,15 +20,6 @@ export async function findAssetsRootsInWorkspace(): Promise<vscode.Uri[]> {
 }
 
 /**
- * Searches root directories for subdirs containing minecraft texture assets
- */
-export async function findTextureAssetsRoots(assetsRoots: vscode.Uri[]) {
-    return assetsRoots
-        .map(root => vscode.Uri.joinPath(root, "minecraft", "textures"))
-        .filter(root => fs.existsSync(root.fsPath));
-}
-
-/**
  * Searches root directories for subdirs containing minecraft model assets
  */
 export async function findModelAssetsRoots(assetsRoots: vscode.Uri[]) {
@@ -39,22 +28,29 @@ export async function findModelAssetsRoots(assetsRoots: vscode.Uri[]) {
         .filter(root => fs.existsSync(root.fsPath));
 }
 
-export async function resolveTextureAssets(assets: string[]) {
-    return resolveAssets(assets, '.png', textureAssetsRoots);
+export async function resolveTextureAssets(resourceLocations: string[]) {
+    return resolveAssets(resourceLocations, "textures", '.png');
 }
 
-export async function resolveModelAssets(assets: string[]) {
-    return resolveAssets(assets, '.json', modelAssetsRoots);
+export async function resolveModelAssets(resourceLocations: string[]) {
+    return resolveAssets(resourceLocations, "models", '.json');
 }
 
-export async function resolveAssets(assets: string[], fileExtension: string, assetsRoots: vscode.Uri[]) {
+export function parseResourceLocation(loc: string) {
+    const parts = loc.split(":");
+    const [namespace, path] = parts.length === 2 ? parts : ["minecraft", parts[0]];
+    return {namespace, path};
+}
+
+export async function resolveAssets(resourceLocations: string[], category: string, fileExtension: string) {
     let resolvedAssets: {[assetPath: string]: vscode.Uri | undefined} = {};
-    for(const assetPath of assets) {
-        resolvedAssets[assetPath] = undefined;
+    for(const resLoc of resourceLocations) {
+        const {namespace, path} = parseResourceLocation(resLoc);
+        resolvedAssets[resLoc] = undefined;
         for(const root of assetsRoots) {
-            const uri = vscode.Uri.joinPath(root, assetPath + fileExtension);
+            const uri = vscode.Uri.joinPath(root, namespace, category, path + fileExtension);
             if(fs.existsSync(uri.fsPath)) {
-                resolvedAssets[assetPath] = uri;
+                resolvedAssets[resLoc] = uri;
                 break;
             }
         }
